dbe104367b865aee25a543c24c0fc1d12d6ffbc7
Mohammed Nasser Ahmed Nasser Bin Ali Jaber <abunasserip@gmail.com>
2023-08-15 19:06:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,30 @@ import streamlit as st
 from langchain.llms import OpenAI
 
 st.title('🦜🔗 chabotSL App')
+st.set_page_config(
+    page_title="My Custom Theme",
+    page_icon=":computer:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
+# Define custom theme
+custom_theme = {
+    "primaryColor": "#7F00FF",
+    "backgroundColor": "#F5F5F5",
+    "secondaryBackgroundColor": "#EBEBEB",
+    "textColor": "#333333",
+    "font": "sans serif"
+}
+
+# Set Streamlit theme with custom theme
+st.set_theme(custom_theme)
+
+# Add elements to Streamlit app
+st.title("My Custom Theme Example")
+st.write("This is an example of a Streamlit app with a custom theme.")
+
+#=============================
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
